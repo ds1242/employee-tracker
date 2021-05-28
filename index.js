0cebc3e22e2585356b01a98794ea6002db01d2b5
dshaw1242 <david.shaw1242@gmail.com>
2021-05-27 19:59:35 -0600

    setup all three prompts to add a role

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const express = require('express');
-// const apiRoutes = require('./routes/apiRoutes');
-// const server = require('./server');
 const inqurier = require('inquirer');
 const cTable = require('console.table');
 
-
-
+const departmentArr = [];
+const getDepartments = () => {
+    const db = require('./db/connection');
+    const sql = `SELECT name FROM department`;
+    
+        db.query(sql, (err, rows) => {
+            if(err) {
+                console.log(err)
+                return;
+            }
+            for(let i = 0; i < rows.length; i++){
+                departmentArr.push(rows[i].name);
+            }
+            
+            console.log(departmentArr);
+            return;
+            
+        });
+};
+// var departmentArr = [];
+getDepartments();
+console.log(departmentArr)
 
 const questions = [
     {
@@ -19,7 +37,58 @@ const questions = [
         type:'input',
         name: 'newDepartment',
         message: 'What is the name of the new department?',
-        when: (answers) => answers.menu === 'Add a department'
+        when: (answers) => answers.menu === 'Add a department',
+        validate: newDepartment => {
+            if(newDepartment) {
+                return true;
+            } else {
+                console.log('Please enter a name for the new department');
+                return false;
+            }
+        }
+    },
+    {
+        type:'input',
+        name: 'newRoleTitle',
+        message: 'What is the title of the new role?',
+        when: (answers) => answers.menu === 'Add a role',
+        validate: newRoleTitle => {
+            if(newRoleTitle) {
+                return true;
+            } else {
+                console.log('Please enter the title of the new role');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'newRoleSalary',
+        message: 'What is the salary for the new role?',
+        when: (answers) => answers.menu === 'Add a role',
+        validate: newRoleSalary => {
+            if(newRoleSalary) {
+                return true;
+            } else {
+                console.log('Please enter a salary for the new role');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'list',
+        name: 'newRoleDepartment',
+        message: 'Please enter a department id this role falls under',
+        choices: departmentArr,
+        when: (answers) => answers.menu === 'Add a role',
+        validate: newRoleDepartment => {
+            if (newRoleDepartment) {
+                return true;
+            } else {
+                console.log('Please enter a department id');
+                return false;
+            }
+        }
     }
 ];
 
@@ -28,7 +97,7 @@ const questions = [
 async function promptUser() {
     const mysql = require('mysql2/promise');
     const db = require('./db/connection');
-
+         
     return inqurier
         .prompt(questions)
         .then(({menu, newDepartment}) => {
@@ -77,11 +146,22 @@ async function promptUser() {
                         console.log(err);
                         return;
                     }
-                    const table = cTable.getTable(rows);
-                    console.log(table);
-                    return promptUser();
+                    const sql = `SELECT * FROM department`;
+                    db.query(sql, (err, rows) => {
+                        if(err) {
+                            console.log(err)
+                            return;
+                        }
+                        const table = cTable.getTable(rows);
+                        console.log(table);
+                        return promptUser();
+                    });
                 })
-            }
+            } 
+            // else if(menu === 'Add a role') {
+            //     const sql = `INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)`
+
+            // }
         })
         // .then(promptUser());
         // .then(db.execute('/api/departments'));
